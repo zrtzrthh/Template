3d559e22eef29a187b8f1e81d1fcecc4914fa3d8
zrt <1814338466@qq.com>
2024-09-24 23:38:10 +0800

    Learn right reference and universal reference

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -1,22 +1,50 @@
 #include <AutoDifferential.h>
 
+class A
+{
+public:
+    A()
+    {
+        std::cout << "construct" << std::endl;
+    }
+    A(A &a)
+    {
+        std::cout << "copy construct" << std::endl;
+    }
+    A(A&& a)
+    {
+        std::cout << "move construct" << std::endl;
+    }
+};
+
+A funcTest()
+{
+    A a;
+    return std::move(a);
+}
+
+
+template<typename T>
+void testForward(T&& v) {}
+
 int 
 main() 
 {
+    // const & 右边可以是lvalue或者rvalue
+    // & 右边只能是lvalue
+    // && 右边只能是rvalue
+    A &&a = funcTest();
+    int c;
+    int && b = 10;
 
-    //AutoDiff<double, double> x(2.0, 0.0); 
-    //x.setDerivative<0>(1.0); 
+    //万能引用，如果一个变量或者参数被声明为T&&，其中T是被推导的类型，那这个变量或者参数就是一个universal reference
+    testForward(1); // case 1
+    int x = 1;
+    testForward(x); // case 2
+    const int& rx = x;
+    testForward(rx); // case 2
 
-    //AutoDiff<double, double> y(0.0, 3.0); 
-    //y.setDerivative<1>(1.0); 
-
-    //AutoDiff<double, double> z = x * x + y * y;
-
-    //std::cout << "f(x, y) = " << std::get<0>(z.values) + std::get<1>(z.values) << std::endl;
-    //std::cout << "df/dx = " << std::get<0>(z.derivatives) << std::endl;
-    //std::cout << "df/dy = " << std::get<1>(z.derivatives) << std::endl;
-
-    print();
 
     return 0;
 }
+
